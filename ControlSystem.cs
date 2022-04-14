2a32f788f6e9d16123a5d4aabf061ccc3f608d76
Wayne George <wayne@lucidpartnership.co.uk>
2022-04-14 12:30:17 +0100

    Poll state testing;

--- ControlSystem.cs
+++ ControlSystem.cs
@@ -15,6 +15,9 @@ namespace TestSharpPro
     public class ControlSystem : CrestronControlSystem
     {
         private GenericComponent levelGetter, levelGetter2;
+        private LevelComponent level1;
+        private StateComponent _state1;
+
         public ControlSystem()
             : base()
         {
@@ -41,31 +44,39 @@ namespace TestSharpPro
         {
             try
             {
-                CrestronConsole.AddNewConsoleCommand(Dsp, "dsp", "send command", ConsoleAccessLevelEnum.AccessOperator);
+                CrestronConsole.AddNewConsoleCommand(Poll, "poll", "send command", ConsoleAccessLevelEnum.AccessOperator);
                 var biamp = new BiampTesira();
                 biamp.Initialize(1);
                 biamp.OnCommunicatingChange += OnCommunicatingChange;
                 biamp.Configure(1, 1, "192.168.2.35", "admin", "admin");
                 biamp.Connect();
 
-                CrestronConsole.PrintLine("SystemStart!");
+                CrestronConsole.PrintLine("SystemStart");
 
-                var state1 = new StateComponent();
-                state1.Configure(1, "Program_Audio", "mute", 1, 0);
+                _state1 = new StateComponent();
+                _state1.Configure(1, "Program_Audio", "mute", 1, 0);
 
                 var state2 = new StateComponent();
                 state2.Configure(1, "Room_Audio", "mute", 1, 0);
 
-                levelGetter = new GenericComponent();
-                levelGetter2 = new GenericComponent();
-                levelGetter.Configure(1, "Program_Audio", "minLevel", 1, 0, 2, 1);
-                levelGetter2.Configure(1, "Program_Audio", "maxLevel", 1, 0, 2, 1);
+                level1 = new LevelComponent();
+                level1.Configure(1, "Program_Audio", "level", 1, 0, 3);
 
-                state1.OnStateChange += State1OnOnStateChange;
+                levelGetter = new GenericComponent();
+                levelGetter.Configure(1, "Program", "mute", 1, 0, 0, 0);
+                
+                //levelGetter2 = new GenericComponent();
+                //levelGetter2.Configure(1, "Program_Audio", "maxLevel", 1, 0, 1, 0);
+
+                _state1.OnStateChange += State1OnOnStateChange;
                 state2.OnStateChange += State1OnOnStateChange;
-                levelGetter.OnSerialChange += LevelGetterMinOnSerialChange;
-                levelGetter.OnAnalogChangeSigned += LevelGetterOnOnAnalogChangeSigned;
-                levelGetter2.OnSerialChange += LevelGetterMaxOnSerialChange;
+                
+                //level1.OnLevelChangeSignedUnscaled += Level1OnOnLevelChangeSignedUnscaled;
+                //level1.OnLevelTextChange += Level1OnOnLevelTextChange;
+                
+                levelGetter.OnDigitalChange += LevelGetterOnOnDigitalChange;
+                //levelGetter.OnAnalogChangeSigned += LevelGetterOnOnAnalogChangeSigned;
+                //levelGetter2.OnAnalogChangeSigned += LevelGetterOnOnAnalogChangeSigned;
                 
 
             }
@@ -76,25 +87,53 @@ namespace TestSharpPro
             return null;
         }
 
+        private void LevelGetterOnOnDigitalChange(object sender, UInt16EventArgs args)
+        {
+            CrestronConsole.PrintLine("LevelGetterOnOnDigitalChange. args:{0}", args.Payload);
+        }
+
+        private void Level1OnOnLevelTextChange(object sender, StringEventArgs args)
+        {
+            var lc = (LevelComponent)sender;
+            CrestronConsole.PrintLine("Level1OnOnLevelTextChange {0}. args:{1}", lc.ConfigInfo.InstanceTag, args.Payload);
+        }
+
+        private void Level1OnOnLevelChangeSignedUnscaled(object sender, Int16EventArgs args)
+        {
+            var lc = (LevelComponent)sender;
+            CrestronConsole.PrintLine("Level1OnOnLevelChangeSignedUnscaled {0}. args:{1}", lc.ConfigInfo.InstanceTag, args.Payload);
+        }
+
         private void LevelGetterOnOnAnalogChangeSigned(object sender, Int16EventArgs args)
         {
-            CrestronConsole.PrintLine("LevelGetterMinOnAnalogChange Int. args:{0}", args.Payload);
+            //var lc = (LevelComponent)sender;
+            CrestronConsole.PrintLine("LevelGetterMinOnAnalogChange {0}.7}", /*lc.ConfigInfo.InstanceTag, */args.Payload);
         }
 
-        private void LevelGetterMaxOnSerialChange(object sender, StringEventArgs args)
-        {
-            CrestronConsole.PrintLine("LevelGetterMaxOnSerialChange. args:{0}", args.Payload);
-        }
-        
-        private void LevelGetterMinOnSerialChange(object sender, StringEventArgs args)
-        {
-            CrestronConsole.PrintLine("LevelGetterMinOnSerialChange. args:{0}", args.Payload);
-        }
-
-        private void Dsp(string cmdparameters)
+        private void Poll(string cmdparameters)
         {
             if (cmdparameters.Length <= 0) return;
-            levelGetter.SetSerial(cmdparameters); // i.e. Program_Audio get minLevel 1
+            switch (cmdparameters)
+            {
+                case "min":
+                    levelGetter.Poll();
+                    break;
+                case "max":
+                    levelGetter2.Poll();
+                    break;
+                case "level":
+                    level1.Poll();
+                    break;
+                case "levels":
+                    level1.PollState();
+                    break;
+                case "state":
+                    levelGetter.Poll();
+                    break;
+                default:
+                    CrestronConsole.PrintLine("Invalid args");
+                    break;
+            }
         }
 
         private void State1OnOnStateChange(object sender, UInt16EventArgs args)
